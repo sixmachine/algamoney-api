11752e0a57aec8873d84d33d0769954be9492973
Unknown <arthur_psena@hotmail.com>
2018-11-05 23:38:37 -0200

    Commit inicial

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,10 @@
+CREATE TABLE CATEGORIA (
+codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO CATEGORIA(nome) values ('Lazer');
+INSERT INTO CATEGORIA(nome) values ('Alimentação');
+INSERT INTO CATEGORIA(nome) values ('Supermercado');
+INSERT INTO CATEGORIA(nome) values ('Farmácia');
+INSERT INTO CATEGORIA(nome) values ('Outros');
