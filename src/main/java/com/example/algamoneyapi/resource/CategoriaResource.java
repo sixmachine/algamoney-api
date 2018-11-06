cec1e9fcf7b4d050a360dafb824fb8ee8de8dec7
Arthur Sena <arthur.psena@gmail.com>
2018-11-06 13:42:51 -0200

    Configuração spring data
    Mapiei a tabela, criei métodos para post e get

---
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -0,0 +1,55 @@
+package com.example.algamoneyapi.resource;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.algamoneyapi.model.Categoria;
+import com.example.algamoneyapi.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository repository;
+
+	@GetMapping
+	public List<Categoria> listarCategorias() {
+		return repository.findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Categoria> categoria = repository.findById(codigo);
+		if(categoria.isPresent()) {
+			return ResponseEntity.ok(categoria.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> salvarCategoria(@RequestBody Categoria categoria) {
+		
+		Categoria salvado = repository.save(categoria);
+		
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{codigo}")
+			.buildAndExpand(salvado.getCodigo())
+			.toUri();
+		
+		return ResponseEntity.created(location).body(salvado);
+	}
+
+}
