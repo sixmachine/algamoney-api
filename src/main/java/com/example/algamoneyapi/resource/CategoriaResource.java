be30b0b24810d58542aa13cd47aa349f8bc0977c
Unknown <arthur_psena@hotmail.com>
2018-11-07 23:43:37 -0200

    Código para realizar o delete

--- src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +28,7 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
@@ -35,24 +36,30 @@ public class CategoriaResource {
 	public List<Categoria> listarCategorias() {
 		return repository.findAll();
 	}
-	
+
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
 		Optional<Categoria> categoria = repository.findById(codigo);
-		if(categoria.isPresent()) {
+		if (categoria.isPresent()) {
 			return ResponseEntity.ok(categoria.get());
 		}
 		return ResponseEntity.notFound().build();
 	}
 
 	@PostMapping
-	public ResponseEntity<Categoria> salvarCategoria(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
-		
+	public ResponseEntity<Categoria> salvarCategoria(@Valid @RequestBody Categoria categoria,
+			HttpServletResponse response) {
+
 		Categoria salvado = repository.save(categoria);
-		
+
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, salvado.getCodigo()));
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(salvado);
+	}
+
+	@DeleteMapping("/{codigo}")
+	public void remover(@PathVariable Long codigo) {
+		repository.deleteById(codigo);
 	}
 
 }
--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoneyapi.event.RecursoCriadoEvent;
@@ -27,7 +29,7 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository repository;
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
@@ -35,26 +37,30 @@ public class PessoaResource {
 	public List<Pessoa> listarPessoas() {
 		return repository.findAll();
 	}
-	
-	
-	@GetMapping
-	@RequestMapping("/{codigo}")
+
+	@GetMapping("/{codigo}")
 	public ResponseEntity<Pessoa> buscarPorCodigo(@PathVariable Long codigo) {
 		Optional<Pessoa> pessoa = repository.findById(codigo);
-		if(pessoa.isPresent()) {
+		if (pessoa.isPresent()) {
 			return ResponseEntity.ok(pessoa.get());
 		}
 		return ResponseEntity.notFound().build();
 	}
 
 	@PostMapping
-	public ResponseEntity<Pessoa> salvarPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
+	public ResponseEntity<Pessoa> salvarPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa salvado = repository.save(pessoa);
-		
+
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, salvado.getCodigo()));
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(salvado);
-		
+
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		repository.deleteById(codigo);
 	}
 
 }
